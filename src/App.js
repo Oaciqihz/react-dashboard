d1cf2e933e24b53fc3159a4e6750b4a181419c44
Caoqizhi <2391580279@qq.com>
2023-08-08 11:41:47 +0800

    en: 添加路由鉴权

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { ProtectedLayout } from './components/ProtectedLayout';
 import HomePage from './views/Home';
 import LoginPage from './views/Login';
 import ProfilePage from './views/Profile';
-import SettingsPage from './views/Settings.js';
+import SettingsPage from './views/Settings';
+import AuthGuard from './components/AuthGuard';
+import TutorialsPage from './views/Tutorials';
 
 function App() {
   return (
@@ -14,10 +16,27 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Route>
-
-      <Route path="/dashboard" element={<ProtectedLayout />}>
-        <Route path="profile" element={<ProfilePage />} />
-        <Route path="settings" element={<SettingsPage />} />
+    
+      <Route 
+        path="/dashboard" 
+        element={
+          <AuthGuard>
+            <ProtectedLayout/>
+          </AuthGuard>
+        }
+      >
+        <Route 
+          path="profile" 
+          element={<ProfilePage />} 
+        />
+        <Route 
+          path="settings" 
+          element={<SettingsPage />} 
+        />
+        <Route 
+          path="tutorials" 
+          element={<TutorialsPage />} 
+        />
       </Route>
     </Routes>
   );
